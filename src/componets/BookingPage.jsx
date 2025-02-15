a0b3ba27f9e7078506952fb2c7103ac3a0496cc7
dev-ankit24 <ankitkumar839282@gmail.com>
2025-02-15 18:45:32 +0530

    ff

--- src/componets/BookingPage.jsx
+++ src/componets/BookingPage.jsx
@@ -22,8 +22,6 @@ export default function BookingPage() {
         </div>
         {/* <!-- Page Header End --> */}
         <Booking/>
-
-        {/* <!-- Booking Start --> */}
         <div class="container-xxl py-5">
             <div class="container">
                 <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
